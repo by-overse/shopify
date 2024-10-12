a71b358c942ea524cfccd754dcfb71cda9f0a156
muchisx <mnm.soul@gmail.com>
2024-10-11 21:29:10 -0500

    feat: setup initial structure

---
+++ .eslintrc.cjs
@@ -0,0 +1,44 @@
+/** @type {import('@types/eslint').Linter.BaseConfig} */
+module.exports = {
+  root: true,
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['react', '@typescript-eslint', 'import'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    project: 'tsconfig.json',
+  },
+  globals: {
+    shopify: 'readonly',
+  },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-floating-promises': 'error',
+    'no-nested-ternary': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-throw-literal': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    'max-len': [
+      'error',
+      {
+        code: 118,
+        ignoreUrls: true,
+        ignoreComments: true,
+        ignorePattern: '^import\\s.+\\sfrom\\s.+;$',
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
+};
