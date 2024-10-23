0a0ea952e587c3e271464345c305ff3ca2d6c3aa
muchisx <mnm.soul@gmail.com>
2024-10-22 19:42:20 -0500

    feat: `ui-extensions-utils` expand extensions constants

--- packages/ui-extensions-utils/dist/constants/targets.js
+++ packages/ui-extensions-utils/dist/constants/targets.js
@@ -4,7 +4,25 @@
 export const CUSTOMER_ACCOUNT_EXTENSION_TARGETS = {
     customer__account_order__index_block_render: 'customer-account.order-index.block.render',
     customer__account_order__status_block_render: 'customer-account.order-status.block.render',
-};
+    customer__account_order__status_cart_line_item_render_after: 'customer-account.order-status.cart-line-item.render-after',
+    customer__account_order__status_cart_line_list_render_after: 'customer-account.order-status.cart-line-list.render-after',
+    customer__account_order__status_customer_information_render_after: 'customer-account.order-status.customer-information.render-after',
+    customer__account_order__status_fulfillment_details_render_after: 'customer-account.order-status.fulfillment-details.render-after',
+    customer__account_order__status_payment_details_render_after: 'customer-account.order-status.payment-details.render-after',
+    customer__account_order__status_return_details_render_after: 'customer-account.order-status.return-details.render-after',
+    customer__account_order__status_unfulfilled_items_render_after: 'customer-account.order-status.unfulfilled-items.render-after',
+    customer__account_order__action_menu_item_render: 'customer-account.order.action.menu-item.render',
+    customer__account_order__action_render: 'customer-account.order.action.render',
+    customer__account_order__page_render: 'customer-account.order.page.render',
+    customer__account_page_render: 'customer-account.page.render',
+    customer__account_profile_addresses_render_after: 'customer-account.profile.addresses.render-after',
+    customer__account_profile_block_render: 'customer-account.profile.block.render',
+    customer__account_profile_company_details_render_after: 'customer-account.profile.company-details.render-after',
+    customer__account_profile_company_location_addresses_render_after: 'customer-account.profile.company-location-addresses.render-after',
+    customer__account_profile_company_location_payment_render_after: 'customer-account.profile.company-location-payment.render-after',
+    customer__account_profile_company_location_staff_render_after: 'customer-account.profile.company-location-staff.render-after',
+    customer__account_profile_payment_render_after: 'customer-account.profile.payment.render-after',
+}; // prettier-ignore
 export const CHECKOUT_EXTENSION_TARGETS = {
     purchase__checkout_block_render: 'purchase.checkout.block.render',
     purchase__checkout_cart_line_item_render_after: 'purchase.checkout.cart-line-item.render-after',
