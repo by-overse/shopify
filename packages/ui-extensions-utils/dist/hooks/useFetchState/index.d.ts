fd1ef04ab30a86a5c4f6536d190d71efb0898c23
muchisx <mnm.soul@gmail.com>
2025-05-02 08:29:30 -0500

    chore: add dist to git ignore

--- packages/ui-extensions-utils/dist/hooks/useFetchState/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function useFetchState<T>(): [FetchState<T>, import("react").Dispatch<import("react").SetStateAction<FetchState<T>>>];
-export type FetchState<T> = {
-    data?: T | null;
-    loading: boolean;
-    error?: unknown | null;
-};
-//# sourceMappingURL=index.d.ts.map
